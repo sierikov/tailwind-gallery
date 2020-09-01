07251941000acc5dd49975dbd992aa0192e82d63
sierikov <artem.sierikov@gmail.com>
2020-09-02 01:56:24 +0300

    Render info from API with ImageCard

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,27 +1,29 @@
 import React from 'react'
 
-const ImageCard = () => {
+const ImageCard = ({ image }) => {
+
+  const tags = image.tags.split(',');
+
   return (
     <div className="max-w-sm my-5 pb-5 rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="https://source.unsplash.com/random?800x600" alt="Random from unsplash"/>
+      <img className="w-full" src={image.webformatURL} alt="Random from unsplash"/>
       <div className="px-6 pt-6">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
+        <div className="font-bold text-xl mb-2">{image.user}</div>
       </div>
-      <div className="px-6 pt-4">
+      <div className="px-6 pt-3">
         <ul>
-          <li><strong>Views: </strong> test</li>
-          <li><strong>Downloads: </strong> 300</li>
-          <li><strong>Likes:</strong> 400</li>
+          <li><strong>Views: </strong> {image.views}</li>
+          <li><strong>Downloads: </strong> {image.downloads}</li>
+          <li><strong>Likes:</strong> {image.likes}</li>
         </ul>
       </div>
       <div className="px-6 pt-4">
-        <span className="mr-2 px-2 inline-flex text-xs leading-4 font-semibold rounded-full bg-gray-200 text-gray-800">#photography</span>
-        <span className="mr-2 px-2 inline-flex text-xs leading-4 font-semibold rounded-full bg-gray-200 text-gray-800">#travel</span>
-        <span className="mr-2 px-2 inline-flex text-xs leading-4 font-semibold rounded-full bg-gray-200 text-gray-800">#winter</span>
-      </div>  
+        {tags.map(tag => (
+          <span key={image.id} className="mr-2 px-2 inline-flex text-xs leading-4 font-semibold rounded-full bg-gray-200 text-gray-800">
+            #{tag}
+          </span> 
+        ))}
+       </div>  
     </div>
   )
 }
