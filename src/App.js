f44430f85d4ae8d8b0a7fc1089116b1f6e173091
sierikov <artem.sierikov@gmail.com>
2020-09-05 18:56:46 +0300

    Fetch refactored

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("");
 
+  const API = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}`;
+  const SEARCH = `&q=${term}`;
+  const TYPE = `&image_type=photo`;
+
   useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
-    )
+    fetch(API + SEARCH + TYPE)
       .then((res) => res.json())
       .then((data) => {
         setImages(data.hits);
         setIsLoading(false);
       })
       .catch((error) => console.error(error));
-  }, [term]);
+  }, [API, SEARCH, TYPE]);
+
   return (
     <div className="container mx-auto my-10">
       <ImageSearch searchText={(text) => setTerm(text)} />
